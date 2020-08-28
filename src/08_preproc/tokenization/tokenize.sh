79bbfc91699d894cb8c5d532036222be036655e8
IML <shino1025@naver.com>
2020-08-28 17:42:04 +0900

    Update tokenize.sh

--- src/08_preproc/tokenization/tokenize.sh
+++ src/08_preproc/tokenization/tokenize.sh
@@ -5,3 +5,6 @@ cut -f1 ${FN} > ${FN}.label
 cut -f2 ${FN} | mecab -O wakati -b 20000 > ${FN}.text.tok
 
 paste ${FN}.label ${FN}.text.tok > ${TARGET_FN}
+
+# echo "아버지가 방에서 나오신다" | mecab
+# echo "아버지가 방에서 나오신다" | mecab -O wakati
