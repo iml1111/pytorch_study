e18dda3beae28cabe54854e2489d5b446dc20cf8
IML <shin10256@gmail.com>
2021-01-08 12:30:34 +0900

    simple-nmt detok 추가

---
+++ src/simple-nmt/data_preparation/detokenizer.py
@@ -0,0 +1,16 @@
+#-*- coding:utf-8 -*-
+import sys
+sys.stdin.reconfigure(encoding='utf-8')
+
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        if line.strip() != "":
+            if '▁▁' in line:
+                line = line.strip().replace(' ', '').replace('▁▁', ' ').replace('▁', '').strip()
+            else:
+                line = line.strip().replace(' ', '').replace('▁', ' ').strip()
+
+            sys.stdout.write(line + '\n')
+        else:
+            sys.stdout.write('\n')
